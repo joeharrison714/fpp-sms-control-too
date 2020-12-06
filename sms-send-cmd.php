7b8d5e141d6a0d4aafbc3b9cba271d2495f5249d
Joe Harrison <github@joeharrison.com>
2020-12-06 16:33:12 -0500

    Update sms-send-cmd.php

--- sms-send-cmd.php
+++ sms-send-cmd.php
@@ -26,11 +26,49 @@ $voipmsApiUsername = returnIfExists($pluginJson, "voipmsApiUsername");
 $voipmsApiPassword = returnIfExists($pluginJson, "voipmsApiPassword");
 $voipmsDid = returnIfExists($pluginJson, "voipmsDid");
 
-//executeSend($number, $message);
+executeSend($number, $message);
 
 function executeSend($number, $message){
     global $voipmsDid;
-    sendMessage($voipmsDid, $number, $message);
+    sendMessageLocal($voipmsDid, $number, $message);
+}
+
+function sendMessageLocal($did, $destination, $message){
+    if (strlen($did) == 0){
+        throw new Exception("No did specified to send from.");
+    }
+
+    if (strlen($destination) == 0){
+        throw new Exception("No destination specified to send to.");
+    }
+
+    if (strlen($message) == 0){
+        throw new Exception("No message specified.");
+    }
+
+    global $apiBasePath,$voipmsApiUsername,$voipmsApiPassword;
+    $url = $apiBasePath . "/rest.php";
+    // $options = array(
+    //     'http' => array(
+    //     'method'  => 'GET'
+    //     )
+    // );
+    // $getdata = http_build_query(
+    //     array(
+    //     'api_username' => $voipmsApiUsername,
+    //     'api_password' => $voipmsApiPassword,
+    //     'method'=>'sendSMS',
+    //     'did'=>$did,
+    //     'dst'=>$destination,
+    //     'message'=>$message
+    //      )
+    // );
+    // logInfo("Sending SMS to: " . $destination);
+    // $context = stream_context_create( $options );
+    // logDebug("API Request: " . $url ."?" .$getdata);
+    // $result = file_get_contents( $url ."?" .$getdata, false, $context );
+    // logDebug("API response: " . $result);
+    // return json_decode( $result );
 }
 
 ?>
